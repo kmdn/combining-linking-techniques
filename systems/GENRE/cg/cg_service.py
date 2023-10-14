c32ac9f04fd6dd52831fbdbc08aa91e532afedca
hashpad <37782032+hashpad@users.noreply.github.com>
2023-10-14 16:54:06 +0200

    Implement cg_service for GENRE

---
+++ systems/GENRE/cg/cg_service.py
@@ -0,0 +1,143 @@
+from flask import Flask, request, Response, jsonify
+import json
+
+import pickle
+
+from genre.fairseq_model import GENRE
+from genre.trie import Trie
+
+app = Flask(__name__)
+
+print("Loading Custom CG")
+
+# load the prefix tree (trie)
+with open("data/kilt_titles_trie_dict.pkl", "rb") as f:
+    trie = Trie.load_from_dict(pickle.load(f))
+
+# load the model
+model = GENRE.from_pretrained("models/fairseq_entity_disambiguation_aidayago").eval()
+
+def add_possible_assignment(score, assignment, possible_assignments_list):
+    possible_assignment_object = {
+            "score": score,
+            "assignment": assignment
+            }
+
+    possible_assignments_list.append(possible_assignment_object)
+
+
+
+
+def generate_candidates(mention, text):
+
+    possible_assignments = []
+    
+    replacement = '[START_ENT] ' + mention['mention'] + ' [END_ENT]'
+    offset = mention['offset']
+    
+
+    print('mention found : ' + mention['mention'] + "\n")
+    new_text = ''
+    new_text = text[:offset]
+    new_text += replacement
+    new_text += text[offset + len(mention['mention']):]
+    print(new_text)
+    res = model.sample(
+        sentences=[new_text],
+        prefix_allowed_tokens_fn=lambda batch_id, sent: trie.get(sent.tolist()),
+    )
+    for candidate in res[0]:
+        print(candidate)
+        possible_assignments.append({"score": candidate['score'].item(), "assignment": candidate['text']})
+    print(res)
+    return possible_assignments
+
+def process(document):
+    # pass mention and text to generator function, generate a text for each mention and then combine the generated candidates
+    mentions = document['mentions']
+    text = document['text']
+
+    for mention in mentions:
+        if mention['possibleAssignments'] is None:
+            mention['possibleAssignments'] = []
+
+        possible_assignments = generate_candidates(mention, text)
+
+
+        for possibleAssignment in possible_assignments:
+            assignment_score = possibleAssignment['score']
+            assignment_value = possibleAssignment['assignment']
+
+            add_possible_assignment(assignment_score, assignment_value, mention['possibleAssignments'])
+
+
+
+
+
+
+# received object: 
+# {
+  # ...
+  # "document": {
+    # "componentId":"input",
+    # "mentions":[
+        # {'mention': 'Napoleon', 'offset': 0},
+        # {'mention': 'emperor', 'offset': 17}
+    # ],
+    # "pipelineType":"NONE",
+    # "text":"Napoleon was the emperor of the First French Empire.", <-- the input text ------------------------------------------------
+    # "uri":null
+  # },
+  # ...
+  # }
+# }
+# curl http://127.0.0.1:5002/ --header "Content-Type: application/json" --request POST -d '{"document":{"uri":null,"text":"Napoleon was the emperor of the First French Empire.","mentions":[{"mention":"Napoleon","offset":0,"assignment":null,"detectionConfidence":-1.0,"possibleAssignments":[],"originalMention":"Napoleon","originalWithoutStopwords":"Napoleon","logger":{"logName":"structure.datatypes.Mention"}},{"mention":"emperor","offset":17,"assignment":null,"detectionConfidence":-1.0,"possibleAssignments":[],"originalMention":"emperor","originalWithoutStopwords":"emperor","logger":{"logName":"structure.datatypes.Mention"}}],"componentId":"MD1","pipelineType":"MD"},"pipelineConfig":{"startComponents":["MD1"],"components":{"cg":[{"id":"CG1","value":"http://127.0.0.1:5002"}],"md":[{"id":"MD1","value":"http://127.0.0.1:5001"}],"cg_ed":[]},"exampleId":"md_combined_cged","endComponents":["CG1"],"displayName":"MD + combined CG-ED","id":1,"connections":[{"source":"MD1","target":"CG1"}],"pipelineConfigType":"complex"},"componentId":"CG1"}'
+
+#
+
+@app.route('/', methods=['get', 'post'])
+def index():
+    print("Incoming request:")
+    print(request.data)
+    req = json.loads(request.data)
+    document = req['document']
+
+    process(document)
+
+    return jsonify(
+            {'document' : document,
+            'pipelineConfig' : req['pipelineConfig'],
+            'componentId' : req['componentId']}
+            )
+
+
+
+class LoggingMiddleware(object):
+    def __init__(self, app):
+        self._app = app
+
+    def __call__(self, env, resp):
+        errorlog = env['wsgi.errors']
+        #pprint.pprint(('REQUEST', env), stream=errorlog)
+
+        def log_response(status, headers, *args):
+            #pprint.pprint(('RESPONSE', status, headers), stream=errorlog)
+            return resp(status, headers, *args)
+
+        return self._app(env, log_response)
+
+
+# Run at flask startup (https://stackoverflow.com/a/55573732)
+with app.app_context():
+    pass
+
+if __name__ == '__main__':
+    port = 5002
+    print("Running app... on port: ", port)
+    app.wsgi_app = LoggingMiddleware(app.wsgi_app)
+    #app.run(host='0.0.0.0', port=80)
+    # expose 0.0.0.0 - esp. important for docker
+    app.run(host='0.0.0.0', port=port)
+    #app.run()
+
+
